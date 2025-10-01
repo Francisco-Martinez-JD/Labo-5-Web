d654a48fd326b752f6d64b1267171f76c4a3a164
Francisco-Martinez-JD <00073424@uca.edu.sv>
2025-09-30 21:18:24 -0600

    Modificar ejercicio final

--- Ejercicio Final/ejerciciofinal/src/Data/scientists.js
+++ Ejercicio Final/ejerciciofinal/src/Data/scientists.js
@@ -1,18 +1,18 @@
-import cena from '../assets/cena.jpg';
-import roman from '../assets/roman.jpg';
+import marie from '../assets/marie.jpg';
+import katsuko from '../assets/katsuko.jpg';
 
 export const scientists = [
   {
-    name: "John Cena",
-    image: cena,
-    profession: "luchador profesional y actor",
-    awards: "25 (16 veces Campeón Mundial, 5 veces Campeón de Estados Unidos, 4 veces Campeón en Parejas)",
-    discoveries: "motivó a millones con su lema 'Never Give Up' y su trabajo con Make-A-Wish Foundation"
+    name: "Maria Skłodowska-Curie",
+    image: marie,
+    profession: "Fisica y química",
+    awards: "4 (Premio Nobel de Física, Premio Nobel de Química, Medalla Davy, Medalla Matteucci)",
+    discoveries: "polonio (elemento quimico) y radio (elemento químico)"
   },
   {
-    name: "Roman Reigns",
-    image: roman,
-    profession: "luchador profesional y ex jugador de fútbol americano",
+    name: "Katsuko Saruhashi",
+    image: katsuko,
+    profession: "Geoquimica",
     awards: "10 (Campeón Universal Indiscutido, Campeón Mundial Peso Pesado, Royal Rumble 2015)",
     discoveries: "estableció el reinado más largo como campeón universal en la era moderna de WWE"
   }
